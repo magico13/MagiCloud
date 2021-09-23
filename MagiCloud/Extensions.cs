ce8fc24196b80a924d5a8fbdb23e8b3d03e73691
magico13 <magico1313@gmail.com>
2021-09-22 20:28:19 -0400

    Line endings

--- MagiCloud/Extensions.cs
+++ MagiCloud/Extensions.cs
@@ -1,9 +1,5 @@
 ﻿using MagiCommon.Models;
 using Microsoft.AspNetCore.Http;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace MagiCloud
