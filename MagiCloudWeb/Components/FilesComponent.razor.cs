5b8fac916790fe186b04ef8cfb6db7edd9439598
magico13 <magico1313@gmail.com>
2021-12-05 17:54:16 -0500

    Start of edit capability

--- MagiCloudWeb/Components/FilesComponent.razor.cs
+++ MagiCloudWeb/Components/FilesComponent.razor.cs
@@ -1,13 +1,16 @@
-﻿using Blazorise.DataGrid.Configuration;
+﻿using Blazorise.DataGrid;
+using Blazorise.DataGrid.Configuration;
 using MagiCommon.Models;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace MagiCloudWeb.Components
 {
     public partial class FilesComponent
     {
+        ElasticFileInfo selectedRow;
         FileList files;
         readonly VirtualizeOptions virtualizeOptions = new()
         {
@@ -43,6 +46,17 @@ namespace MagiCloudWeb.Components
         public async Task FilesChanged()
         {
             await GetFilesAsync();
+        }
+
+        public void RowRemoved(ElasticFileInfo file)
+        {
+            Logger.LogInformation("Removing file {Name}", file.Name);
+            //await MagicApi.RemoveFileAsync(file.Id);
+        }
+
+        public void RowUpdated(SavedRowItem<ElasticFileInfo, Dictionary<string, object>> saved)
+        {
+            Logger.LogInformation("Updating file {Name}", saved.Item.Name);
         }
     }
 }
