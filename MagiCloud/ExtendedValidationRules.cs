75056f99b8315e23721038a617529a579dfe4458
magico13 <magico1313@gmail.com>
2022-12-03 22:34:10 -0500

    Add modal to remote file download. Centralize some validators

---
+++ MagiCloud/ExtendedValidationRules.cs
@@ -0,0 +1,49 @@
+﻿using Blazorise;
+using MagiCommon;
+
+namespace MagiCloud;
+
+public class ExtendedValidationRules
+{
+    public static void IsURI(ValidatorEventArgs e)
+    {
+        var urlString = e.Value as string;
+        var valid = Validators.IsValidURI(urlString);
+        if (valid is null)
+        {
+            e.Status = ValidationStatus.None;
+        }
+        else
+        {
+            e.Status = valid.Value ? ValidationStatus.Success : ValidationStatus.Error;
+        }
+    }
+
+    public static void IsFileName(ValidatorEventArgs e)
+    {
+        var fileName = e.Value as string;
+        var valid = Validators.IsValidFileName(fileName);
+        if (valid is null)
+        {
+            e.Status = ValidationStatus.None;
+        }
+        else
+        {
+            e.Status = valid.Value ? ValidationStatus.Success : ValidationStatus.Error;
+        }
+    }
+
+    public static void IsFilePath(ValidatorEventArgs e)
+    {
+        var filePath = e.Value as string;
+        var valid = Validators.IsValidFilePath(filePath);
+        if (valid is null)
+        {
+            e.Status = ValidationStatus.None;
+        }
+        else
+        {
+            e.Status = valid.Value ? ValidationStatus.Success : ValidationStatus.Error;
+        }
+    }
+}
---
+++ MagiCommon/Validators.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MagiCommon
+{
+    public static class Validators
+    {
+        public static bool? IsValidURI(string uri) => string.IsNullOrWhiteSpace(uri) ? null : (bool?)Uri.TryCreate(uri, UriKind.Absolute, out var _);
+        public static bool? IsValidFileName(string fileName)
+        {
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                return null;
+            }
+
+            var invalidChars = Path.GetInvalidPathChars().ToList();
+            invalidChars.AddRange(new List<char>() { '<', '>', ':', '"', '/', '|', '?', '*', '\\' });
+            return fileName.IndexOfAny(invalidChars.ToArray()) < 0; // -1 means not found
+        }
+
+        public static bool? IsValidFilePath(string filePath)
+        {
+            if (string.IsNullOrWhiteSpace(filePath))
+            {
+                return null;
+            }
+
+            var fileName = filePath.Split('/').Last();
+            var validFileName = IsValidFileName(fileName);
+            if (!validFileName.GetValueOrDefault())
+            {
+                return false;
+            }
+
+            var invalidChars = Path.GetInvalidPathChars().ToList();
+            invalidChars.AddRange(new List<char>() { '<', '>', ':', '"', '|', '?', '*', '\\' });
+            invalidChars.Remove('/'); // allow /
+            return filePath.IndexOfAny(invalidChars.ToArray()) < 0; // -1 means not found
+        }
+    }
+}
