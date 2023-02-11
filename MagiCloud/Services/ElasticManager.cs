5a94fa07b8e64d225cdade92973c3d0cdc4141f6
magico13 <magico1313@gmail.com>
2023-02-11 16:40:46 -0500

    Remove info message that writes a lot

--- MagiCloud/Services/ElasticManager.cs
+++ MagiCloud/Services/ElasticManager.cs
@@ -72,7 +72,6 @@ public class ElasticManager : IElasticManager
         if (!string.IsNullOrWhiteSpace(_settings.ClientCertPath))
         {
             var cert = new X509Certificate2(_settings.ClientCertPath);
-            _logger.LogInformation("Using ca cert for Elastic communication defined at {Path}", _settings.ClientCertPath);
             connectionSettings.ClientCertificate(cert);
             connectionSettings.ServerCertificateValidationCallback(CertificateValidations.AllowAll);
             // TODO: Find a better way to do this
